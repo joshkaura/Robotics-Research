4edd40487793881969ac9f47a9af6172e080c4b1
joshkaura <josh.kaura02@gmail.com>
2025-12-11 20:42:26 +0000

    convert scale to mm, save output

--- WeldSeamTracking.py
+++ WeldSeamTracking.py
@@ -17,15 +17,15 @@ class LaserScanGenerator:
     """
     Simple simulator for laser scans.
 
-    - Each scan has `scan_length` values.
+    - Each scan has fixed scan length.
     - First seam position can be anywhere.
-    - For all *subsequent* scans, the seam index is forced to be within
-      `max_offset_from_centre` indices of the torch centre index.
+    - For subsequent scans, the seam index is forced to be within
+      a max offset from the torch centre index.
     """
 
     def __init__(
         self,
-        scan_length: int = 61,
+        scan_length: int = 101,
         base_distance: float = 10.0,
         seam_depth: float = 3.0,
         noise_std: float = 0.3,
@@ -44,22 +44,22 @@ class LaserScanGenerator:
         # Seam can start anywhere for the first scan
         self.current_seam_index = np.random.randint(0, self.scan_length)
 
-    def _choose_followup_seam_index(self, centre_index: int):
+    def _choose_next_seam_index(self, last_seam_pos: int):
         # For subsequent scans, constrain seam index near the centre
-        low = max(0, centre_index - self.max_offset_from_centre)
-        high = min(self.scan_length - 1, centre_index + self.max_offset_from_centre)
+        low = max(0, last_seam_pos - self.max_offset_from_centre)
+        high = min(self.scan_length - 1, last_seam_pos + self.max_offset_from_centre)
         self.current_seam_index = np.random.randint(low, high + 1)
 
-    def generate_scan(self, centre_index: int):
+    def generate_scan(self, last_pos: int):
         """
-        Generate a single scan given the current torch centre index.
+        Generate a scan given the torch centre is above the centre of the laser FOV .
         Returns (scan, true_seam_index).
         """
         if self._first_scan:
             self._first_scan = False
             self._choose_initial_seam_index()
         else:
-            self._choose_followup_seam_index(centre_index)
+            self._choose_next_seam_index(last_pos)
 
         # Start with roughly flat surface + noise
         scan = self.base_distance + np.random.normal(
@@ -72,11 +72,23 @@ class LaserScanGenerator:
         return scan, self.current_seam_index
 
 class LaserSensor:
-    def __init__(self, scan_length = 61):
+    def __init__(self, scan_length = 101, fov_half_width_mm = 50.0):
         self.scan_length = scan_length
         self.sensor_centre_index = (scan_length - 1)//2
         self.feature_x = 0 #x index of the feature in the scan data (perpendicular to the welding path)
         self.feature_y = 0 #y value (distance away from the sensor) of the feature - not essential if welding piece assumed flat
+
+        #fov_half_width_mm: half of FOV in mm (e.g. 20mm each side => [-20, +20])
+        self.fov_half_width_mm = fov_half_width_mm
+
+        self.x_profile_mm = np.linspace(-(self.fov_half_width_mm), fov_half_width_mm, scan_length)
+
+        self.feature_index = 0 #x index of the feature in the scan data (perpendicular to the welding path)
+        self.feature_x_mm = 0.0
+        self.feature_y = 0.0 #y value (distance away from the sensor) of the feature
+
+        
+
     
     def clean_data(self, scan_data):
 
@@ -105,47 +117,51 @@ class LaserSensor:
 
         # for this example, find min value as the feature to be tracked
         self.feature_y = np.min(self.cleaned_data)
-        self.feature_x = np.argmin(self.cleaned_data)
+        self.feature_index = int(np.argmin(self.cleaned_data))
+        self.feature_x_mm = float(self.x_profile_mm[self.feature_index])
 
-        return self.feature_x, self.feature_y
+
+        return self.feature_index, self.feature_x_mm, self.feature_y
 
 
 class WeldingController:
-    def __init__(self, sensor: LaserSensor, max_offset_indices: int = 4):
+    def __init__(self, sensor: LaserSensor, max_offset_mm: float = 4.0):
         self.sensor = sensor
-        self.max_offset_indices = max_offset_indices
+        self.max_offset_mm = max_offset_mm
         self.torch_offset = 0.0 # offset of the torch from the seam feature (perpendicular distance w.r.t weld path)
         self.torch_position = 0.0 #position in robot "world"
-        self.feature_x = None
+
+        self.feature_index = None
+        self.feature_x_mm = None
         self.feature_y = None
         self.welding = True
 
-        self.path=[] #path as list of (scan_index, torch_pos)
+        self.path=[] #path as list of (scan_index, torch_pos(mm))
 
     def calc_torch_offset(self):
-        # Calculate the offset of the torch from the seam feature
-        index_offset = self.feature_x - self.sensor.sensor_centre_index
-        self.torch_offset = index_offset
-        return index_offset
+        # Calculate the offset of the torch from the seam feature - assuming torch at centre of laser FOV (0mm)
+        offset_mm = self.feature_x_mm # seam pos mm - 0
+        self.torch_offset = offset_mm
+        return offset_mm
 
     def process_scan(self, scan_data, scan_index):
         # Find seam feature
-        self.feature_x, self.feature_y = self.sensor.find_feature(scan_data)
-        print(f"Feature found at X: {self.feature_x}, Y: {self.feature_y:.3f}")
+        self.feature_index, self.feature_x_mm, self.feature_y = self.sensor.find_feature(scan_data)
+        print(f"Feature found at X (mm): {self.feature_x_mm:.2f}, Y: {self.feature_y:.3f}")
 
 
-        offset_indices = self.calc_torch_offset()
+        offset_mm = self.calc_torch_offset()
         print(
-            f"Torch scan centre index = {self.sensor.sensor_centre_index}, "
-            f"offset = {offset_indices} indices"
+            f"Torch at X=0 of sensor FOV"
+            f"Torch offset required = {offset_mm} indices"
         )
 
         #Stopping Criterion:
-        if abs(offset_indices) <= self.max_offset_indices:
-            self.torch_position += offset_indices
+        if abs(offset_mm) <= self.max_offset_mm:
+            self.torch_position += offset_mm
             print(
-                f"Torch moves by {offset_indices} index units. "
-                f"New torch_position (index units) = {self.torch_position}"
+                f"Torch moves by {offset_mm} mm "
+                f"New torch X position (mm) = {self.torch_position}"
             )
         else:
             self.stop_recentre()
@@ -158,25 +174,30 @@ class WeldingController:
         #welding stops if seam too far from torch centre
         self.welding = False
 
-        print(f"Welding stopped - seam {self.feature_x} is more than {self.max_offset_indices} indices from centre {self.sensor.sensor_centre_index}")
+        print(
+            f"***WELDING STOPPED***"
+            f"Seam at {self.feature_x_mm}mm is more than {self.max_offset_mm} mm from current torch position"
+        )
 
-        # Reposition: move torch centre index to seam index
-        offset_indices = self.torch_offset
-        self.torch_position += offset_indices
+        # Reposition: move torch to seam pos
+        offset_mm = self.torch_offset
+        self.torch_position += offset_mm
+
+        time.sleep(1)
 
         print(
-            f"Robot repositions by {offset_indices} units.\n"
+            f"Robot repositions by {offset_mm} units.\n"
             f"New torch position = {self.torch_position}"
         )
 
         # After repositioning, welding resumed
         self.welding = True
-        print("Welding Resumed")
+        print("***WELDING RESUMED***")
 
     def __repr__(self):
         return (
-            f"Torch offset (indices): {self.torch_offset}, "
-            f"Torch position (indices): {self.torch_position}, "
+            f"Torch offset (indices): {self.torch_offset:.3f}, "
+            f"Torch position (indices): {self.torch_position:.2f}, "
             f"Welding: {self.welding}"
         )
     
@@ -188,13 +209,13 @@ class PathPlotter:
 
         self.ax.set_xlim(*xlim)
         self.ax.set_ylim(*ylim)
-        self.ax.set_xlabel("Torch position (index units)")
+        self.ax.set_xlabel("Torch position (mm)")
         self.ax.set_ylabel("Scan number")
         self.ax.set_title("Robot Welding Path")
 
         # scatter plot initially empty
-        self.scatter = self.ax.scatter([], [])
-        self.line, = self.ax.plot([], [], '-o', color='red', linewidth=1.5)
+        self.scatter = self.ax.scatter([], [], s=1)
+        self.line, = self.ax.plot([], [], '-', color='red', linewidth=0.5)
 
     def update(self, path):
         """
@@ -215,18 +236,32 @@ class PathPlotter:
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
+    def save(self, filepath="Results/WeldSeamTracking.png", dpi=300):
+        self.fig.savefig(filepath, dpi=dpi, bbox_inches="tight")
+        print(f"Saved final plot to: {filepath}")
+
     def show_blocking(self):
         plt.ioff()
         plt.show()
     
+    
 if __name__ == "__main__":
-    # Create sensor and generator
-    sensor = LaserSensor(scan_length=61)
-    generator = LaserScanGenerator(scan_length=sensor.scan_length)
-    robot = WeldingController(sensor)
+    #choose scan_length (no. lds points in scan)
+    scan_length = 101
+    #choose laser FOV
+    fov_half_width_mm = 20.0
+    #choose maximum seam position offset (mm) to continue welding continuously
+    max_offset_mm = 5.0
 
-    n_scans = 20
-    plotter = PathPlotter(xlim=(-40,40), ylim=(0,n_scans))
+    # Create sensor and scan generator
+    sensor = LaserSensor(scan_length=scan_length, fov_half_width_mm=fov_half_width_mm)
+    generator = LaserScanGenerator(scan_length=sensor.scan_length)
+
+    # initialise welding robot
+    robot = WeldingController(sensor, max_offset_mm=max_offset_mm)
+
+    n_scans = 100
+    plotter = PathPlotter(xlim=(-(fov_half_width_mm + 20),fov_half_width_mm+20), ylim=(0,n_scans))
 
     # Simulate Scans
     for i in range(n_scans):
@@ -239,6 +274,9 @@ if __name__ == "__main__":
         #update path plot
         plotter.update(robot.path)
 
-        time.sleep(0.5)
+        time.sleep(0.05)
 
+    plotter.save()
     plotter.show_blocking()
+    plt.close()
+
