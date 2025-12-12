16f838b14dc42bd415e8f09c16b6e92eda4aea42
joshkaura <josh.kaura02@gmail.com>
2025-12-12 19:46:32 +0000

    get average distacne error per scan

--- laser_robot_calibration.py
+++ laser_robot_calibration.py
@@ -25,10 +25,11 @@ def rotate_z(point, angle):  # r
 
 # Calculate estimated reference point
 def get_robot_frame_coord(tcp_pos, laser_output, sensor_frame):
-    #laser_output = [0, -laser_output[1], -laser_output[2]]
+
+    laser_output = [0, -laser_output[1], -laser_output[2]]
 
     #testing laser coordinate switches
-    laser_output = [laser_output[2], -laser_output[1], 0]
+    #laser_output = [laser_output[2], -laser_output[1], 0]
 
 
     # First rotation stage - rotate predicted laser offset (x,y,z of guessed sensor frame)
@@ -64,10 +65,13 @@ def optimisation_target(params, data):
         computed_coords = get_robot_frame_coord(tcp_pos, laser_output, sensor_frame)
         #update total error
         total_error += euclidean_distance(computed_coords, observed_data)
-    #print(total_error)
-    return total_error
 
-# Gradient descent algorithm for minimisation - set learning rate and iterations as required
+    #get average error per scan
+    avg_error = total_error / len(data)
+    #print(total_error)
+    return total_error, avg_error
+
+# Gradient descent algorithm for minimisation (finding sensor frame) - set learning rate and iterations as required
 def optimise_params_gd(initial_guess, data, learning_rate=0.01, iterations = 30000):
     # Initialise start point (guess) - base it roughly on sensor position on robot arm
     params = initial_guess
@@ -77,9 +81,9 @@ def optimise_params_gd(initial_guess, data, learning_rate=0.01, iterations = 300
         for i in range(len(params)):
             delta = 1e-5
             params[i] += delta
-            error_plus = optimisation_target(params, data)
+            error_plus, _ = optimisation_target(params, data)
             params[i] -= 2 * delta
-            error_minus = optimisation_target(params, data)
+            error_minus, _ = optimisation_target(params, data)
             params[i] += delta
             gradients[i] = (error_plus - error_minus) / (2 * delta)
         #update new best estimate for sensor frame values
@@ -88,6 +92,7 @@ def optimise_params_gd(initial_guess, data, learning_rate=0.01, iterations = 300
 
 
 '''
+# brute force optimisation algorithm 
 def optimise_params_bf(data):
     x_list = np.arange(30, 70, step=0.1)
     y_list = np.arange(-10, 10, step=0.1)
@@ -114,25 +119,7 @@ def optimise_params_bf(data):
     return best_params
 '''
 
-
-'''
-#Actual reference point
-ref1 = [1319.715, -36.597, 117.90]
-
-
-#Sample data
-data = [
-    (ref1, [1264.111, -38.164, 162.019], [0, 0, 0], [0, -5.47, 5.41]),
-    (ref1, [1266.098, -50.641, 161.951], [0, 0, 0], [0, 5.78, 6.89]),
-    (ref1, [1271.982, -38.164, 177.187], [0, 0, 0], [0, -4.27, -9.99]),
-    (ref1, [1273.998, -52.084, 177.187], [0, 0, 0], [0, 8.44, -9.03]),
-    (ref1, [1268.548, -43.712, 168.077], [0, 0, 0], [0, -0.25, 0.03]),
-    (ref1, [1269.730, -53.504, 168.126], [0, 0, 0], [0, 9.21, 0.50]),
-]
-'''
-
-'''
-#Sample CW data:
+#Sample Data:
 ref1 = [1065.01, 371.82, -143.10]
 data = [
     (ref1, [1057.73, 291.74, -150.99, -1.84, 7.66, 89.41], [0, 16.44, -0.87]),
@@ -140,38 +127,14 @@ data = [
     (ref1, [1085.52, 292.18, -150.99, -1.84, 7.66, 89.41], [0, -9.84, -1.76]),
     (ref1, [1056.58, 311.48, -127.65, -1.83, 7.66, 89.41], [0, 16.64, 29.46]),
 ]
-'''
-
-'''
-#Sample OSL Data
-ref1 = [1519.65, -415.29, 203.17]
-data = [
-    (ref1, [1465.48, -403.29, 238.42, 0, 0, 0], [0, 7.64, -7.42]),
-    (ref1, [1464.33, -420.57, 238.42, 0, 0, 0], [0, -9.17, -7.72]),
-    (ref1, [1471.93, -420.57, 254.74, 0, 0, 0], [0, -9.29, 8.76]),
-    (ref1, [1473.04, -401.55, 254.65, 0, 0, 0], [0, 9.50, 8.85]),
-]
-'''
-
-
-#Sample RR F Data
-ref1 = [777.38, 396.39, 950.95]
-data = [
-    (ref1, [785.82, 386.15, 953.79, -87.62, 0, 0], [0, 0.31, 2.86]),
-    (ref1, [785.84, 385.81, 960.89, -87.62, 0, 0], [0, -7.34, 2.59]),
-    (ref1, [765.51, 396.34, 960.87, -87.62, 0, 0], [0, -6.98, 25.49]),
-    (ref1, [765.53, 396.39, 953.79, -87.62, 0, 0], [0, 0.03, 25.63]),
-]
 
 
 #Initialise guess
-initial_guess = [-50, 5, 0, 0, -25, 0]
+initial_guess = [0, 0, 0, 0, 0, 0]
 # Run gradient descent algorithm
 optimised_params = optimise_params_gd(initial_guess, data)
 # Get the total error for the calculated sensor frame values
-total_error = optimisation_target(optimised_params, data)
+total_error, avg_error = optimisation_target(optimised_params, data)
 print(f"Sensor Frame: x,y,z - {optimised_params[:3]} ; w,p,r - {optimised_params[3:]}")
-print(f"Total Error: {total_error}")
-
-#brute force
-#best_params = optimise_params_bf(data)
+print(f"Total Error (mm): {total_error}")
+print(f"Average Error (mm): {avg_error}")
